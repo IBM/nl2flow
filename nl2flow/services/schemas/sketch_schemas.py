61e9ee5a34155a5a1e46775711334cba9b677765
TathagataChakraborti <tathagata.net@live.com>
2022-12-26 18:07:33 -0500

    stash
    Signed-off-by: TathagataChakraborti <tathagata.net@live.com>

---
+++ nl2flow/services/schemas/sketch_schemas.py
@@ -0,0 +1,6 @@
+# from typing import Set, List, Optional, Union
+from pydantic import BaseModel
+
+
+class Sketch(BaseModel):
+    pass
---
+++ nl2flow/services/sketch.py
@@ -0,0 +1,18 @@
+# from nl2flow.compile.flow import Flow
+# from nl2flow.compile.operators import ClassicalOperator as Operator
+# from nl2flow.compile.schemas import SignatureItem
+# from nl2flow.plan.schemas import PlannerResponse
+
+
+class SketchCompilation:
+    pass
+
+    # def setup_method(self) -> None:
+    #     self.flow = Flow(name="Basic Sketch")
+
+    # def compile(
+    #     self,
+    # ) -> PlannerResponse:
+    # plans: PlannerResponse = self.flow.plan_it(self.planner)
+    # print(self.planner.pretty_print(plans))
+    # return plans
