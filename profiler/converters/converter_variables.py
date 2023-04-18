c4d03c6c9520df1cbf06d5d84cc183d9096f20e3
Jung koo Kang <jungkoo.kang@ibm.com>
2023-04-18 14:39:14 -0400

    Create  info_2_flow_converter

---
+++ profiler/converters/converter_variables.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+AGENT_ID = "agent_id"
+ACTUATOR_SIGNATURE = "actuator_signature"
+IN_SIGNATURE = "in_sig_full"
+OUT_SIGNATURE = "out_sig_full"
+SIGNATURE_TYPES: List[str] = [IN_SIGNATURE, OUT_SIGNATURE]
+SEQUENCE_ALIAS = "sequence_alias"
+SLOT_FILLABLE = "slot_fillable"
--- profiler/converters/info_2_flow_converter.py
+++ profiler/converters/info_2_flow_converter.py
@@ -1,8 +1,11 @@
 from typing import List, Set, Tuple
+from nl2flow.compile.flow import Flow
 from profiler.generators.info_generator.agent_info_data_types import AgentInfo, Plan
 from nl2flow.compile.operators import Operator
+from nl2flow.compile.options import GoalType
 from nl2flow.compile.schemas import (
     MemoryItem,
+    MemoryState,
     SignatureItem,
     MappingItem,
     GoalItems,
@@ -10,34 +13,121 @@ from nl2flow.compile.schemas import (
     SlotProperty,
     MappingItem,
 )
+from profiler.converters.converter_variables import (
+    AGENT_ID,
+    ACTUATOR_SIGNATURE,
+    SIGNATURE_TYPES,
+    IN_SIGNATURE,
+    SEQUENCE_ALIAS,
+    SLOT_FILLABLE,
+)
+from uuid import uuid4
 
 
-def get_available_agents_in_flow(
-    available_agents: List[AgentInfo],
-) -> Tuple[List[Operator], List[SlotProperty]]:
-    return get_operators_for_flow(available_agents), get_slot_fillers_for_flow(
-        available_agents
-    )
+def get_uuid() -> str:
+    return str(uuid4())
 
 
 def get_operators_for_flow(available_agents: List[AgentInfo]) -> List[Operator]:
-    return []
+    # TODO: TEST THIS
+    operators: List[Operator] = list()
+    for agent_info in available_agents:
+        operator = Operator(agent_info[AGENT_ID])
+        if ACTUATOR_SIGNATURE in agent_info:
+            for signature_type in SIGNATURE_TYPES:
+                if (
+                    signature_type in agent_info[ACTUATOR_SIGNATURE]
+                    and agent_info[ACTUATOR_SIGNATURE][signature_type] is not None
+                ):
+                    signature_names: List[str] = list()
+                    for signature_item in agent_info[ACTUATOR_SIGNATURE][
+                        signature_type
+                    ]:
+                        signature_names.append(signature_item[SEQUENCE_ALIAS][:])
+
+                    if signature_type == IN_SIGNATURE:
+                        operator.add_input(SignatureItem(parameters=signature_names))
+                    else:
+                        operator.add_output(SignatureItem(parameters=signature_names))
+        operators.append(operator)
+    return operators
 
 
 def get_goals_for_flow(goals: Set[str]) -> GoalItems:
-    # TODO: COMPLETE THIS
-    return GoalItems()
+    # TODO: TEST THIS
+    goals: List[GoalItem] = list()
+    for goal_name in goals:
+        goals.append(GoalItem(goal_name=goal_name, goal_type=GoalType.OPERATOR))
+
+    return GoalItems(goals=goals)
 
 
 def get_slot_fillers_for_flow(available_agents: List[AgentInfo]) -> List[SlotProperty]:
-    # TODO: COMPLETE THIS
-    return []
+    # TODO: TEST THIS
+    slot_properties: List[SlotProperty] = list()
+    for agent_info in available_agents:
+        if ACTUATOR_SIGNATURE in agent_info:
+            for signature_type in SIGNATURE_TYPES:
+                if (
+                    signature_type in agent_info[ACTUATOR_SIGNATURE]
+                    and agent_info[ACTUATOR_SIGNATURE][signature_type] is not None
+                ):
+                    for signature_item in agent_info[ACTUATOR_SIGNATURE][
+                        signature_type
+                    ]:
+                        if (
+                            SLOT_FILLABLE in signature_item
+                            and signature_item[SLOT_FILLABLE]
+                        ):
+                            slot_properties.append(
+                                SlotProperty(slot_name=signature_item[SEQUENCE_ALIAS])
+                            )
+
+    return slot_properties
 
 
 def get_data_mappers_for_flow(
     mappings: List[Tuple[str, str, float]]
 ) -> List[MappingItem]:
-    return []
+    # TODO: TEST THIS
+    return list(
+        map(
+            lambda mapping: MappingItem(
+                source_name=mapping[0], target_name=mapping[1], probability=mapping[2]
+            ),
+            mappings,
+        )
+    )
+
+
+def get_available_data_for_flow(available_data: List[str]) -> List[MemoryItem]:
+    # TODO: TEST THIS
+    return list(
+        map(
+            lambda signature_item_name: MemoryItem(
+                item_id=signature_item_name, item_state=MemoryState.KNOWN
+            ),
+            available_data,
+        )
+    )
+
+
+def get_flow_from_agent_infos(
+    available_agents: List[AgentInfo],
+    mappings: List[Tuple[str, str, float]],
+    goals: Set[str],
+    available_data: List[str],
+) -> Flow:
+    # TODO: TEST THIS
+    flow = Flow(get_uuid())
+    flow.add(
+        get_operators_for_flow(available_agents)
+        + get_slot_fillers_for_flow(available_agents)
+        + get_data_mappers_for_flow(mappings)
+        + get_available_data_for_flow(available_data)
+        + get_goals_for_flow(goals)
+    )
+    return flow
 
 
 def get_pddl_plan_str(plan: Plan) -> str:
