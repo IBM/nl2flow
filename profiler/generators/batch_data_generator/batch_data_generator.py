750c49907c54ed0bc22825ebfdeb2641bc6df30e
Jung koo Kang <jungkoo.kang@gmail.com>
2024-03-11 11:09:07 -0400

    Create batch LLM training data generator

--- profiler/generators/batch_data_generator/batch_data_generator.py
+++ profiler/generators/batch_data_generator/batch_data_generator.py
@@ -44,64 +44,36 @@ def get_agent_info_generator_inputs(
 def get_pddl_generator_output_batch(
     batch_input: AgentInfoGeneratorInputBatch, planner: Planner, random: ModuleType
 ) -> Generator[List[PddlGeneratorOutput], None, None]:
-    # TODO: TEST THIS
     used_settings: Set[int] = set()
-    for num_agents in batch_input.num_agents:
-        for num_input_parameters in batch_input.num_input_parameters:
-            for num_var in batch_input.num_var:
-                if num_var < num_input_parameters * 2:
-                    continue
-                for num_goal_agents in batch_input.num_goal_agents:
-                    if num_goal_agents > num_agents:
-                        continue
-                    for proportion_coupled_agents in batch_input.proportion_coupled_agents:
-                        for proportion_mappable_variables in batch_input.proportion_mappable_variables:
-                            for prop_slot_fillable_variables in batch_input.proportion_slot_fillable_variables:
-                                if (prop_slot_fillable_variables < num_input_parameters) or (
-                                    prop_slot_fillable_variables > (num_input_parameters * 2) * num_agents
-                                ):
-                                    continue
-                                for num_var_types in batch_input.num_var_types:
-                                    for slot_filler_option in batch_input.slot_filler_option:
-                                        for name_generator in batch_input.name_generator:
-                                            generator_input_setting_hash = AgentInfoGeneratorInputCheck(
-                                                num_agents=num_agents,
-                                                num_var=num_var,
-                                                num_input_parameters=num_input_parameters,
-                                                num_samples=batch_input.num_samples,
-                                                num_goal_agents=num_goal_agents,
-                                                num_coupled_agents=int(num_agents * proportion_coupled_agents),
-                                                num_slot_fillable_variables=int(num_var * prop_slot_fillable_variables),
-                                                num_mappable_variables=int(num_var * proportion_mappable_variables),
-                                                num_var_types=num_var_types,
-                                                slot_filler_option=slot_filler_option,
-                                                name_generator=name_generator,
-                                                error_message=None,
-                                            ).__hash__()
-
-                                            if generator_input_setting_hash in used_settings:
-                                                continue
-
-                                            used_settings.add(generator_input_setting_hash)
-
-                                            try:
-                                                input = AgentInfoGeneratorInput(
-                                                    num_agents=num_agents,
-                                                    num_var=num_var,
-                                                    num_input_parameters=num_input_parameters,
-                                                    num_samples=batch_input.num_samples,
-                                                    num_goal_agents=num_goal_agents,
-                                                    proportion_coupled_agents=proportion_coupled_agents,
-                                                    proportion_slot_fillable_variables=prop_slot_fillable_variables,
-                                                    proportion_mappable_variables=proportion_mappable_variables,
-                                                    num_var_types=num_var_types,
-                                                    slot_filler_option=slot_filler_option,
-                                                )
-                                                pddl_generator_outputs = generate_dataset_with_info_generator(
-                                                    agent_info_generator_input=input, planner=planner, random=random
-                                                )
-                                                if pddl_generator_outputs is not None:
-                                                    yield pddl_generator_outputs
-                                            except Exception as e:
-                                                print(e)
-                                                continue
+    for agent_info_generator_input in get_agent_info_generator_inputs(batch_input):
+        generator_input_setting_hash = AgentInfoGeneratorInputCheck(
+            num_agents=agent_info_generator_input.num_agents,
+            num_var=agent_info_generator_input.num_var,
+            num_input_parameters=agent_info_generator_input.num_input_parameters,
+            num_samples=batch_input.num_samples,
+            num_goal_agents=agent_info_generator_input.num_goal_agents,
+            num_coupled_agents=int(
+                agent_info_generator_input.num_agents * agent_info_generator_input.proportion_coupled_agents
+            ),
+            num_slot_fillable_variables=int(
+                agent_info_generator_input.num_var * agent_info_generator_input.prop_slot_fillable_variables
+            ),
+            num_mappable_variables=int(
+                agent_info_generator_input.num_var * agent_info_generator_input.proportion_mappable_variables
+            ),
+            num_var_types=agent_info_generator_input.num_var_types,
+            slot_filler_option=agent_info_generator_input.slot_filler_option,
+            name_generator=agent_info_generator_input.name_generator,
+            error_message=agent_info_generator_input.error_message,
+        ).__hash__()
+        if generator_input_setting_hash in used_settings:
+            continue
+        used_settings.add(generator_input_setting_hash)
+        try:
+            pddl_generator_outputs = generate_dataset_with_info_generator(
+                agent_info_generator_input=agent_info_generator_input, planner=planner, random=random
+            )
+            if pddl_generator_outputs is not None:
+                yield pddl_generator_outputs
+        except Exception as e:
+            print(e)
