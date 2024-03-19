ebf12fdd26eba95dd2d55a7820e353c8f91aaaa1
Jung koo Kang <jungkoo.kang@gmail.com>
2024-03-19 09:24:32 -0400

    Remove type ignore

--- profiler/validators/agent_info_generator_test_utils.py
+++ profiler/validators/agent_info_generator_test_utils.py
@@ -14,7 +14,7 @@ def get_stats_coupled_agents(
     variable_agent_dict_outs: Dict[str, Set[str]] = dict()
     for agent_info in agent_infos:
         for signature in SIGNATURE_TYPES:
-            for item in agent_info["actuator_signature"][signature]:  # type: ignore
+            for item in agent_info["actuator_signature"][signature]:
                 if signature == "in_sig_full":
                     if item["name"] not in variable_agent_dict_ins:
                         variable_agent_dict_ins[item["name"]] = set()
@@ -47,7 +47,7 @@ def get_num_slot_fillable_variables(agent_infos: List[AgentInfo]) -> int:
     slot_fillable_var: Set[str] = set()
     for agent_info in agent_infos:
         for signature in SIGNATURE_TYPES:
-            for item in agent_info["actuator_signature"][signature]:  # type: ignore
+            for item in agent_info["actuator_signature"][signature]:
                 if item["slot_fillable"]:
                     slot_fillable_var.add(item["name"])
 
@@ -58,7 +58,7 @@ def get_num_variables(agent_infos: List[AgentInfo], available_data: List[Tuple[s
     vars: Set[str] = set()
     for agent_info in agent_infos:
         for signature in SIGNATURE_TYPES:
-            for item in agent_info["actuator_signature"][signature]:  # type: ignore
+            for item in agent_info["actuator_signature"][signature]:
                 vars.add(item["name"][:])
     for datum in available_data:
         vars.add(datum[0][:])  # add variable name
@@ -78,7 +78,7 @@ def get_num_variables_used_for_data_mapping(mappings: List[Tuple[(str, str, floa
 def check_num_input_parameters(agent_infos: List[AgentInfo], num_input_parameters: int) -> bool:
     for agent_info in agent_infos:
         for signature in SIGNATURE_TYPES:
-            if num_input_parameters != len(agent_info["actuator_signature"][signature]):  # type: ignore
+            if num_input_parameters != len(agent_info["actuator_signature"][signature]):
                 return False
 
     return True
@@ -109,6 +109,7 @@ def check_sample(
     expected_prop_variables_mapping = 0 if expected_prop_variables_mapping == 1 else expected_prop_variables_mapping
     assert expected_prop_variables_mapping == get_num_variables_used_for_data_mapping(mappings)
     # slot-filler
-    assert ceil(input.num_var * input.proportion_slot_fillable_variables) == get_num_slot_fillable_variables(
+    # variables assigned for available data can't be checked if they are slot-fillable
+    assert ceil(input.num_var * input.proportion_slot_fillable_variables) >= get_num_slot_fillable_variables(
         available_agents
     )
