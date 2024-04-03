d5f0cdc6734d09bfa6ee43479032a9ebd86d79ea
Jung koo Kang <jungkoo.kang@gmail.com>
2024-04-03 07:26:10 -0400

    Enhance profiling without plans

--- profiler/generators/dataset_generator/dataset_generator.py
+++ profiler/generators/dataset_generator/dataset_generator.py
@@ -39,7 +39,7 @@ def generate_dataset_with_info_generator(
                 pddl_domain=trim_pddl_str(pddl.domain, pddl_start_key),
                 pddl_problem=trim_pddl_str(pddl.problem, pddl_start_key),
                 list_of_plans=planner_response.list_of_plans,
-                prettified_plans=planner.pretty_print(planner_response),
+                prettified_plans=planner.pretty_print(planner_response) if should_plan else "",
                 sample_hash=sample.get_hash(),
                 agent_info_generator_input=agent_info_generator_input.model_copy(deep=True),
                 compiler_planner_lag_millisecond=compiler_planner_lag,
