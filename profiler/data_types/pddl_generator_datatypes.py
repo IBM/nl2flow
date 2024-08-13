a4715a3aa19864d226b95a0d66a030e6cfb0778e
Jung koo Kang <jungkoo.kang@gmail.com>
2024-08-13 12:40:04 -0400

    Return tags at PddlGeneratorOutput

--- profiler/data_types/pddl_generator_datatypes.py
+++ profiler/data_types/pddl_generator_datatypes.py
@@ -1,8 +1,21 @@
+from math import ceil
 from pydantic import BaseModel
 from typing import List
 from nl2flow.plan.schemas import ClassicalPlan, PlannerResponse
 from profiler.data_types.generator_data_type import AgentInfoGeneratorInput
 from profiler.data_types.generator_output_data_type import AgentInfoGeneratorOutputItem
+
+
+class PlanningDatumTag(BaseModel):
+    number_of_agents: int = -1
+    number_of_variables: int = -1
+    parameters_per_agent: int = -1
+    coupling_of_agents: float = -1.0
+    enable_slots: bool = False
+    enable_maps: bool = False
+    multiple_goals: bool = False
+    objects_in_memory: bool = False
+    length_of_sequence: int = -1
 
 
 class PddlGeneratorOutput(BaseModel):
@@ -17,3 +30,25 @@ class PddlGeneratorOutput(BaseModel):
     compiler_planner_lag_millisecond: float  # lag in millisecond
     planner_response: PlannerResponse  # planner response
     agent_info_generator_output_item: AgentInfoGeneratorOutputItem  # seed for flow object
+
+    def get_tags(self) -> PlanningDatumTag:
+        num_agents = len(self.agent_info_generator_output_item.available_agents)
+        return PlanningDatumTag(
+            number_of_agents=num_agents,
+            number_of_variables=self.agent_info_generator_input.num_var,
+            parameters_per_agent=(self.agent_info_generator_input.num_input_parameters * 2),
+            coupling_of_agents=(ceil(num_agents * self.agent_info_generator_input.proportion_coupled_agents) * 100.0),
+            enable_slots=(
+                True
+                if ceil(
+                    self.agent_info_generator_input.num_var
+                    * self.agent_info_generator_input.proportion_slot_fillable_variables
+                )
+                > 0
+                else False
+            ),
+            enable_maps=(True if len(self.agent_info_generator_output_item.mappings) > 0 else False),
+            multiple_goals=(True if len(self.agent_info_generator_output_item.goal_agent_ids) > 1 else False),
+            objects_in_memory=(True if len(self.agent_info_generator_output_item.available_data) > 0 else False),
+            length_of_sequence=(len(self.list_of_plans[0].plan) if len(self.list_of_plans) > 0 else 0),
+        )
