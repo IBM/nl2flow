f86114009f3177026b6adcc5fb5e6d63e149fff0
Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>
2024-11-14 15:46:04 -0500

    parameter parse with quoted text
    Signed-off-by: Tathagata-Chakraborti1 <tathagata.chakraborti1@ibm.com>

--- tests/planner/formatting/test_codelike_print.py
+++ tests/planner/formatting/test_codelike_print.py
@@ -224,3 +224,15 @@ class TestCodeLikePrint:
 
         assert parse_parameters(signature="agent_a(b,c)") == ("agent_a", ["b", "c"])
         assert parse_parameters(signature="3a-1(b-2,c3)") == ("3a-1", ["b-2", "c3"])
+
+        assert parse_parameters(signature='a("b,",c)') == ("a", ['"b, "', "c"])
+        assert parse_parameters(signature='a("b,", "x", c)') == ("a", ['"b, "', '"x"', "c"])
+        assert parse_parameters(signature='a(a, "b,","x",c)') == ("a", ["a", '"b, "', '"x"', "c"])
+
+        assert parse_parameters(signature='a("b,x,c",c)') == ("a", ['"b, x, c"', "c"])
+        assert parse_parameters(signature='a(a, "b,x,c", "c,y",z)') == ("a", ["a", '"b, x, c"', '"c, y"', "z"])
+
+        assert parse_parameters(signature='a("Pets called "Mary" in "Sydney", Australia",c)') == (
+            "a",
+            ['"Pets called "Mary" in "Sydney", Australia"', "c"],
+        )
